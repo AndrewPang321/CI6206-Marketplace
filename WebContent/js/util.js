124593dcb38e95a4cac1e5800683d9475b03a730
AndrewPang <thpangab@connect.ust.hk>
2018-09-11 21:35:06 +0800

    Fixed indentation

--- WebContent/js/util.js
+++ WebContent/js/util.js
@@ -5,17 +5,17 @@
  * { message: "displayText", class: "alertClassFromBootstrap" }
  */
 function showAlert(alertObj) {
-	var html = `<div class="alert alert-${alertObj.class} alert-dismissible fade" role="alert" id="alert">`;
-	html += `${alertObj.message}`;
-	html += `<button type="button" class="close" data-dismiss="alert" aria-label="Close">`;
+    var html = `<div class="alert alert-${alertObj.class} alert-dismissible fade" role="alert" id="alert">`;
+    html += `${alertObj.message}`;
+    html += `<button type="button" class="close" data-dismiss="alert" aria-label="Close">`;
     html += `<span aria-hidden="true">&times;</span>`;
     html += `</button>`;
     html += `</div>`;
-    
+
     $("#alertBox").append(html);
     // The alert lasts for 2s
     $("#alert").fadeTo(2000, 500).slideUp(500, () => {
-    	$("#success-alert").slideUp(500);
-    	$("#alert").alert("close");
+        $("#success-alert").slideUp(500);
+        $("#alert").alert("close");
     });
 }
