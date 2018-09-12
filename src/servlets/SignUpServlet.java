95b08d8050f4b1822e38c1869b1920453ee87e30
AndrewPang <thpangab@connect.ust.hk>
2018-09-12 15:17:46 +0800

    Fixed indentation and minor implementation

--- src/servlets/SignUpServlet.java
+++ src/servlets/SignUpServlet.java
@@ -18,37 +18,37 @@ public class SignUpServlet extends HttpServlet {
      * @see HttpServlet#HttpServlet()
      */
     public SignUpServlet() {
-	super();
-	// TODO Auto-generated constructor stub
+        super();
+        // TODO Auto-generated constructor stub
     }
 
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	// TODO Auto-generated method stub
-	response.getWriter().append("Served at: ").append(request.getContextPath());
+        // TODO Auto-generated method stub
+        response.getWriter().append("Served at: ").append(request.getContextPath());
     }
 
     /**
      * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	String email = request.getParameter("signupEmail");
-	String password = request.getParameter("signupPassword");
-	String confirmPassword = request.getParameter("signupConfirmPassword");
+        String email = request.getParameter("signupEmail");
+        String password = request.getParameter("signupPassword");
+        String confirmPassword = request.getParameter("signupConfirmPassword");
 
-	if ((email != "" && password != "") && (password.equals(confirmPassword))) {
-	    // TODO: Perform registration with DB
-	    Boolean success = true;
-	    if (success) {
-		// Sign up success, 201: Created
-		response.setStatus(201);
-	    } else {
-		// Sign up fail, 400: Bad Request
-		response.setStatus(400);
-	    }
-	}
+        if (("".equals(email) && "".equals(password)) && (password.equals(confirmPassword))) {
+            // TODO: Perform registration with DB
+            Boolean success = true;
+            if (success) {
+                // Sign up success, 201: Created
+                response.setStatus(201);
+            } else {
+                // Sign up fail, 400: Bad Request
+                response.setStatus(400);
+            }
+        }
     }
 
 }
