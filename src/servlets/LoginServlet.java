4f0216c519433d7b63d5e6732c8579f3a487eb28
AndrewPang <thpangab@connect.ust.hk>
2018-09-12 15:15:59 +0800

    Fixed indentation and minor implementation

--- src/servlets/LoginServlet.java
+++ src/servlets/LoginServlet.java
@@ -18,36 +18,36 @@ public class LoginServlet extends HttpServlet {
      * @see HttpServlet#HttpServlet()
      */
     public LoginServlet() {
-	super();
-	// TODO Auto-generated constructor stub
+        super();
+        // TODO Auto-generated constructor stub
     }
 
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	// TODO Auto-generated method stub
-	response.getWriter().append("Served at: ").append(request.getContextPath());
+        // TODO Auto-generated method stub
+        response.getWriter().append("Served at: ").append(request.getContextPath());
     }
 
     /**
      * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	String email = request.getParameter("loginEmail");
-	String password = request.getParameter("loginPassword");
+        String email = request.getParameter("loginEmail");
+        String password = request.getParameter("loginPassword");
 
-	if (email != "" && password != "") {
-	    // TODO: Successful authentication with DB
-	    Boolean success = true;
-	    if (success) {
-		// Authentication success, 200: Success
-		response.setStatus(200);
-	    } else {
-		// Authentication fail, 400: Bad Request
-		response.setStatus(400);
-	    }
-	}
+        if ("".equals(email) && "".equals(password)) {
+            // TODO: Successful authentication with DB
+            Boolean success = true;
+            if (success) {
+                // Authentication success, 200: Success
+                response.setStatus(200);
+            } else {
+                // Authentication fail, 400: Bad Request
+                response.setStatus(400);
+            }
+        }
     }
 
 }
