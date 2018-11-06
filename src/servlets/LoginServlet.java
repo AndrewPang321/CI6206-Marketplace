4e0622d6008b649f84c78b1813eadce8a7de6fbd
AndrewPang <thpangab@connect.ust.hk>
2018-11-06 17:42:28 +0800

    Handled login with salted pw checking

--- src/servlets/LoginServlet.java
+++ src/servlets/LoginServlet.java
@@ -11,6 +11,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.mindrot.jbcrypt.BCrypt;
 
 /**
  * Servlet implementation class LoginServlet
@@ -21,73 +22,34 @@ public class LoginServlet extends HttpServlet {
 
     public LoginServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
+        // Auto-generated method stub
         response.getWriter().append("Served at: ").append(request.getContextPath());
-
-
-//        User loginUser = userRepository.findByEmail(email);
-//        if (loginUser == null) {
-//            // Authentication fail, 400: Bad Request
-//            redirectAttributes.addFlashAttribute("display", "show");
-//            redirectAttributes.addFlashAttribute("msg", "Incorrect email");
-//            return "redirect:/auth";
-//        }
-//
-//        if (BCrypt.checkpw(password, loginUser.getUserAccount().getPassword())) {
-//            // Authentication success, 200: Success
-//            saveUserSession(session, loginUser);
-//            return "redirect:/";
-//        }
-//
-//        // Authentication fail, 400: Bad Request
-//        redirectAttributes.addFlashAttribute("display", "show");
-//        redirectAttributes.addFlashAttribute("msg", "Incorrect password");
-//        return "redirect:/auth";
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String email = request.getParameter("loginEmail");
         String password = request.getParameter("loginPassword");
-        // set headers and buffer size before accessing the Writer
-        response.setContentType("text/html");
-        response.setBufferSize(8192);
 
-        PrintWriter out = response.getWriter();
-
-        if (!"".equals(email) && !"".equals(password)) {
-            // TODO: Successful authentication with DB
-            try {
+        try {
+            if (!"".equals(email) && !"".equals(password)) {
                 AccountDBAO accountDB = new AccountDBAO();
                 User user = accountDB.getUser(email);
-                // If can't get, user == null
                 if (user != null) {
-                    // Check password
-                    // Save into Session?
+                    if (BCrypt.checkpw(password, user.getUserAccount().getPassword())) {
+                        // TODO: Save into Session
+                        // Authentication success, 200: Success
+                        response.setStatus(200);
+                        return;
+                    }
                 }
-                System.out.println("TEST DB 2: " + user);
-                System.out.println("TEST DB 2: " + user.getEmail());
-                System.out.println("TEST DB 2: " + user.getUserAccount());
-                System.out.println("TEST DB 2: " + user.getUserAccount().getPassword());
-//                out.print("<h2>" + user.getEmail() + "</h2>");
-            } catch (Exception ex) {
-                response.resetBuffer();
-                throw new ServletException(ex);
             }
-//            Boolean success = true;
-//            if (success) {
-//                // Authentication success, 200: Success
-//                response.setStatus(200);
-//            }
-//            else {
-//                // Authentication fail, 400: Bad Request
-//                response.setStatus(400);
-//            }
+        } catch (Exception ex) {
+            throw new ServletException(ex);
         }
-        out.close();
+
         // Authentication fail, 400: Bad Request
         response.setStatus(400);
     }
