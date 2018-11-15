853638d114bbbc6a970792b769f18e6903b1baf5
TobyGitster <43654069+TobyGitster@users.noreply.github.com>
2018-11-16 00:55:09 +0800

    Add files via upload

---
+++ src/servlets/UserProfileServlet.java
@@ -0,0 +1,63 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+import database.DBAO;
+import database.Item;
+import database.User;
+
+/**
+ * Servlet implementation class UserProfile
+ */
+@WebServlet("/userProfile")
+public class UserProfile extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserProfile() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// Auto-generated method stub
+		int user_id = User.currentUser.getUserId();
+
+        try {
+            DBAO DB = new DBAO();
+            User user = DB.getCurrentUserProfile(user_id);
+            request.setAttribute("User", user);
+            response.setStatus(200);
+        } catch (Exception ex) {
+            response.setStatus(400);
+            throw new ServletException(ex);
+        }
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/userProfile.jsp");
+        dispatcher.forward(request, response);
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
